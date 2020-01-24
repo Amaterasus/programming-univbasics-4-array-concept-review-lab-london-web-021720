5731c4ae768ec2afd3d0da0b43e8b9f876cf74fa
Skye Geraghty <skyetay03@googlemail.com>
2020-01-24 13:52:02 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,12 @@
 def find_element_index(array, value_to_find)
+  output = nil
   
+  array.each do |value|
+    if value == value_to_find
+      output = value
+    end
+  end
+  return output
 end
 
 def find_max_value(array)
